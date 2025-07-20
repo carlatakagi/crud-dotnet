6b23eb49e8d5d06b49994520a87160c172552b4a
carlatakagi <carlatakagi@gmail.com>
2025-07-20 18:23:26 -0400

    feature: add new route

---
+++ MyFirstApi/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApi.Controllers;
+
+
+[Route("[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetUser()
+    {
+        var response = new Response
+        {
+            Name = "Taylor",
+            Age = "35"
+        };
+
+        return Ok(response);
+    }
+}
---
+++ MyFirstApi/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace MyFirstApi
+{
+    public class Response
+    {
+        public string Name { get; set; }
+        public string Age { get; set; }
+    }
+}
