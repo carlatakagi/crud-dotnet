8af8e800cf9737f4c25cb7af5045c994a65d06c2
carlatakagi <carlatakagi@gmail.com>
2025-07-20 18:42:01 -0400

    feature: update get user route

--- MyFirstApi/Controllers/UserController.cs
+++ MyFirstApi/Controllers/UserController.cs
@@ -3,15 +3,19 @@
 namespace MyFirstApi.Controllers;
 
 
-[Route("[controller]")]
+[Route("api/[controller]")]
 [ApiController]
 public class UserController : ControllerBase
 {
     [HttpGet]
-    public IActionResult GetUser()
+    [Route("{id}")]
+    [ProducesResponseType(typeof(Response), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    public IActionResult GetUser(int id)
     {
         var response = new Response
         {
+            Id = 1,
             Name = "Taylor",
             Age = "35"
         };
