242ec4265672fc354e9b43360d1cc1615c08d252
carlatakagi <carlatakagi@gmail.com>
2025-07-20 21:28:50 -0400

    feature: rename route

--- MyFirstApi/Controllers/UserController.cs
+++ MyFirstApi/Controllers/UserController.cs
@@ -11,7 +11,7 @@ public class UserController : ControllerBase
     [Route("{id}")]
     [ProducesResponseType(typeof(Response), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
-    public IActionResult GetUser(int id)
+    public IActionResult GetUserById(int id)
     {
         var response = new Response
         {
